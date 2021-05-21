9d952b87644164db3f4d8e67ca6ebd0d2a537da2
Smudger1 <mugoftea37@gmail.com>
2021-05-21 18:58:18 +0100

    Update HistoryScreen.js

--- mobile_app/src/screens/HistoryScreen.js
+++ mobile_app/src/screens/HistoryScreen.js
@@ -1,9 +1,11 @@
 import React, {useState}  from 'react';
 import { StyleSheet, ScrollView, View, TouchableOpacity  } from 'react-native';
-import { IndexPath, Layout, Text, Card, Button, Select, SelectItem, TopNavigation, TopNavigationAction  } from '@ui-kitten/components';
+import { List, Text, Card, TopNavigation, TopNavigationAction } from '@ui-kitten/components';
 import { Icon } from 'react-native-eva-icons';
 
 import {styles} from '../styles'
+
+import { CheckInUtil } from "../../util/CheckInUtil";
 
 const PinIcon = (props) => (
     <Icon name='pin-outline' width={30} height={30} fill='#000' {...props} />
@@ -13,60 +15,118 @@ const BackIcon = (props) => (
     <Icon name='arrow-back' width={24} height={24} fill='#000' {...props} />
 );
 
+class HistoryPopulate extends React.Component{
+    constructor(props) {
+        super(props);
+        this.temp = {
+            history: null
+        }
+        this.state = {
+            history: null
+        }
+        this.forceUpdateHandler = this.forceUpdateHandler.bind(this);
+    }
+
+    forceUpdateHandler(){
+        this.forceUpdate();
+    }
+
+    componentDidMount() {
+        this.getHistory();
+    }
+
+    async getHistory(){
+        const checkIns = new CheckInUtil()
+
+        await checkIns.getCheckInHistory().then(r => this.state.history = r);
+
+        this.forceUpdateHandler();
+    }
+
+    renderItemHeader = (headerProps, venueName) => (
+        <View {...headerProps}>
+            <Text category="h4"><PinIcon/> {venueName}</Text>
+        </View>
+    );
+
+    renderItem = (checkIn) => (
+        <Card
+            style={styles.historyCard}
+            header={headerProps => this.renderItemHeader(headerProps, checkIn.item.beacon.venue.venueName)}>
+            <Text category='h6'>Date: {new Date(checkIn.item.dateIn).toLocaleDateString()}</Text>
+            <Text style={styles.historyCardTime} category='h5'>Checked-In:
+                {" " + new Date(checkIn.item.dateIn).toLocaleTimeString()}</Text>
+            <Text style={styles.historyCardTime} category='h5'>Checked-Out:
+                {checkIn.item.dateOut === null ? " N/A" : " " + new Date(checkIn.item.dateOut).toLocaleTimeString()}</Text>
+            <Text>No Current Positive Exposure</Text>
+        </Card>
+    )
+
+    render() {
+
+        let historyCards = null;
+
+        console.log("state", this.state.history);
+
+        if (this.state.history && this.state.history.length > 0 ) {
+            this.state.history.reverse(); // Reverse order of data
 
 
 
-const HistoryScreen = ({ navigation }) => {
+            historyCards = (
+                <List
+                    data={this.state.history}
+                    renderItem={this.renderItem}
+                />
+            )
 
-    const navigateBack = () => {
-        navigation.goBack();
+        } else {
+            historyCards = (
+                <Card style={styles.historyCard}>
+                    <Text category='h6'>No Previous Check Ins Found.</Text>
+                </Card>
+            );
+        }
+
+        return (
+            <>
+                {historyCards}
+            </>
+        );
+
+
+    }
+}
+
+
+class HistoryScreen extends React.Component {
+    constructor(props) {
+        super(props);
+        this.navigation = props.navigation
     };
 
-    const BackAction = () => (
-        <TopNavigationAction icon={BackIcon} onPress={navigateBack}/>
+    navigateBack = () => {
+        this.navigation.goBack();
+    };
+
+    BackAction = () => (
+        <TopNavigationAction icon={BackIcon} onPress={this.navigateBack}/>
     );
 
-    const Header = (props) => (
-        <View {...props}>
-            <Text category="h4"><PinIcon/> The One Eyed Dog</Text>
-        </View>
-    );
+    render() {
+        return (
+            <View style={styles.navContainer}>
+                <TopNavigation
+                    accessoryLeft={this.BackAction}
+                    title='Check-in History'
+                />
+                <ScrollView style={styles.containerWNav}>
+                    <HistoryPopulate />
+                </ScrollView>
+            </View>
+        );
+    }
 
-
-    return(
-        <View style={styles.navContainer}>
-            <TopNavigation
-                accessoryLeft={BackAction}
-                title='Check-in History'
-            />
-            <ScrollView style={styles.containerWNav}>
-                <Card style={styles.historyCard} header={Header}>
-                    <Text category='h6'>Date: dd/mm/yyyy</Text>
-                    <Text style={styles.historyCardTime} category='h5'>Checked-In: hh:mm</Text>
-                    <Text style={styles.historyCardTime} category='h5'>Checked-Out: hh:mm</Text>
-                    <Text>No Current Positive Exposure</Text>
-                </Card>
-                <Card style={[styles.historyCard, styles.historyCardPosExposure]} header={Header}>
-                    <Text category='h6'>Date: dd/mm/yyyy</Text>
-                    <Text style={styles.historyCardTime} category='h5'>Checked-In: hh:mm</Text>
-                    <Text style={styles.historyCardTime} category='h5'>Checked-Out: hh:mm</Text>
-                    <Text style={styles.historyCardPosExposureText}>Positive Exposure Registered</Text>
-                </Card>
-                <Card style={styles.historyCard} header={Header}>
-                    <Text category='h6'>Date: dd/mm/yyyy</Text>
-                    <Text style={styles.historyCardTime} category='h5'>Checked-In: hh:mm</Text>
-                    <Text style={styles.historyCardTime} category='h5'>Checked-Out: hh:mm</Text>
-                    <Text>No Current Positive Exposure</Text>
-                </Card>
-                <Card style={styles.historyCard} header={Header}>
-                    <Text category='h6'>Date: dd/mm/yyyy</Text>
-                    <Text style={styles.historyCardTime} category='h5'>Checked-In: hh:mm</Text>
-                    <Text style={styles.historyCardTime} category='h5'>Checked-Out: hh:mm</Text>
-                    <Text>No Current Positive Exposure</Text>
-                </Card>
-            </ScrollView>
-        </View>
-    );
 }
 
 export default HistoryScreen;
